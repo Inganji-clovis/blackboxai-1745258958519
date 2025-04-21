21e5add8f5743347ef731fdef7fdc83a93d53ffc
inganji clovis fiscal <160033673+Inganji-clovis@users.noreply.github.com>
2025-04-21 11:34:19 -0700

    Add backend/app/Models/Group.php 1745260452477

---
+++ backend/app/Models/Group.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'teacher_id',
+    ];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+
+    public function students()
+    {
+        return $this->belongsToMany(User::class, 'group_student', 'group_id', 'student_id');
+    }
+}
